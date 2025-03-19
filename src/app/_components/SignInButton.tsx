e691a994ad75e453af77f01e5c7b2dbfc6a5abea
Andreas Rossa <andreas@rossamail.de>
2025-03-19 01:02:42 +0100

    Cleanup

--- src/app/_components/SignInButton.tsx
+++ src/app/_components/SignInButton.tsx
@@ -1,11 +1,31 @@
 'use client'
 
+import { Button } from "@/components/ui/button"
 import { authClient } from "@/lib/auth-client"
+import Image from "next/image"
 
 export function SignInButton() {
     const { data } = authClient.useSession()
-    return <div className="flex flex-col gap-2">
-        <button onClick={() => authClient.signIn.social({ provider: "discord" })}>Sign in with Discord</button>
-        <span>Signed in as {data?.user?.email}</span>
+  return (
+    <div className="flex flex-col gap-2 items-center">
+      { !data?.user && (
+        <Button onClick={() => authClient.signIn.social({ provider: "discord" })}>
+          Sign in with Discord
+        </Button>
+      )}
+      <span>Signed in as {data?.user?.email}</span>
+      {data?.user && (
+        <Button onClick={() => authClient.signOut()}>Sign out</Button>
+      )}
+      {data?.user?.image && (
+        <Image
+          src={data.user.image}
+          alt="User avatar"
+          className="w-10 h-10 rounded-full"
+          width={40}
+          height={40}
+        />
+      )}
     </div>
-}  
+  );
+}
