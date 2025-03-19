3882a77667c3b67c8a0a595e192d3bc5d67f506c
Andreas Rossa <andreas@rossamail.de>
2025-03-19 01:19:00 +0100

    add venue router to appRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { replayRouter } from "@/server/api/routers/replay";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
+import { venueRouter } from "./routers/venue";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   replay: replayRouter,
+  venue: venueRouter,
 });
 
 // export type definition of API
