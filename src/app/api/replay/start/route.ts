d6ff5ceda8510d21f0dc1fa6e5f8aa9073e89222
Andreas Rossa <andreas@rossamail.de>
2025-03-19 01:01:27 +0100

    add api route for replay start mutation

---
+++ src/app/api/replay/start/route.ts
@@ -0,0 +1,29 @@
+import { type AppRouter, createCaller } from "@/server/api/root";
+import { createTRPCContext } from "@/server/api/trpc";
+import { TRPCError } from "@trpc/server";
+import {
+  getHTTPStatusCodeFromError,
+  type inferProcedureInput,
+} from "@trpc/server/unstable-core-do-not-import";
+import { type NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const ctx = await createTRPCContext({ headers: request.headers });
+  const caller = createCaller(ctx);
+
+  try {
+    const body = (await request.json()) as inferProcedureInput<
+      AppRouter["replay"]["start"]
+    >;
+    const replay = await caller.replay.start(body);
+    return NextResponse.json(replay);
+  } catch (cause) {
+    if (cause instanceof TRPCError) {
+      const status = getHTTPStatusCodeFromError(cause);
+      return NextResponse.json(cause.message, { status });
+    }
+
+    console.error(cause);
+    return NextResponse.json("Internal Server Error", { status: 500 });
+  }
+}
