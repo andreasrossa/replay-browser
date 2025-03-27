b9dd8b4e663df9ca712b71c273872225919a55fa
Andreas Rossa <andreas@rossamail.de>
2025-03-27 22:32:09 +0100

    Add better error handling for collector update, regenerate secret and create mutation

--- src/server/api/routers/collector.ts
+++ src/server/api/routers/collector.ts
@@ -32,6 +32,17 @@ export const collectorRouter = createTRPCRouter({
   create: adminProcedure
     .input(createCollectorSchema)
     .mutation(async ({ ctx: { db }, input }) => {
+      const exists = await db.query.collector.findFirst({
+        where: eq(collector.uid, input.uid),
+      });
+
+      if (exists) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Collector already exists",
+        });
+      }
+
       const secret = generateSecret();
       const hashedSecret = hashSecret(secret);
       const [createdCollector] = await db
@@ -69,20 +80,28 @@ export const collectorRouter = createTRPCRouter({
         });
       }
 
-      const [updatedCollector] = await db
-        .update(collector)
-        .set({ displayName: input.displayName })
-        .where(eq(collector.uid, input.uid))
-        .returning();
+      try {
+        const [updatedCollector] = await db
+          .update(collector)
+          .set({ displayName: input.displayName })
+          .where(eq(collector.uid, input.uid))
+          .returning();
 
-      if (!updatedCollector) {
+        if (!updatedCollector) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Failed to update collector",
+          });
+        }
+
+        return updatedCollector;
+      } catch (error) {
+        console.error(error);
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
           message: "Failed to update collector",
         });
       }
-
-      return updatedCollector;
     }),
   regenerateSecret: adminProcedure
     .input(z.object({ uid: z.string() }))
@@ -98,31 +117,39 @@ export const collectorRouter = createTRPCRouter({
         });
       }
 
-      const newSecret = generateSecret();
-      const hashedSecret = hashSecret(newSecret);
+      try {
+        const newSecret = generateSecret();
+        const hashedSecret = hashSecret(newSecret);
 
-      const [updatedCollector] = await db
-        .update(collector)
-        .set({
-          secret: hashedSecret,
-          secretExpiresAt: new Date(
-            Date.now() + env.COLLECTOR_SECRET_EXPIRATION_TIME_SECONDS * 1000,
-          ),
-        })
-        .where(eq(collector.uid, input.uid))
-        .returning();
+        const [updatedCollector] = await db
+          .update(collector)
+          .set({
+            secret: hashedSecret,
+            secretExpiresAt: new Date(
+              Date.now() + env.COLLECTOR_SECRET_EXPIRATION_TIME_SECONDS * 1000,
+            ),
+          })
+          .where(eq(collector.uid, input.uid))
+          .returning();
 
-      if (!updatedCollector) {
+        if (!updatedCollector) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Collector not found",
+          });
+        }
+
+        return {
+          secret: newSecret,
+          secretExpiresAt: updatedCollector.secretExpiresAt,
+        };
+      } catch (error) {
+        console.error(error);
         throw new TRPCError({
-          code: "BAD_REQUEST",
-          message: "Collector not found",
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to regenerate collector secret",
         });
       }
-
-      return {
-        secret: newSecret,
-        secretExpiresAt: updatedCollector.secretExpiresAt,
-      };
     }),
   delete: adminProcedure
     .input(z.object({ uid: z.string() }))
