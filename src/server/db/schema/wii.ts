78020844c22bd912f745a8f79f092f8d6d4332f9
Andreas Rossa <andreas@rossamail.de>
2025-04-05 22:52:04 +0200

    add mac and ip validation in Wii schema and add collectorUID index

--- src/server/db/schema/wii.ts
+++ src/server/db/schema/wii.ts
@@ -1,11 +1,26 @@
-import { pgTable, text } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import { check, index, pgTable, text } from "drizzle-orm/pg-core";
 import { collector } from "./collector";
 
-export const wii = pgTable("wii", {
-  macAddress: text("mac_address").primaryKey(),
-  nickname: text("nickname").notNull(),
-  ipAddress: text("ip_address").notNull(),
-  collectorUID: text("collector_uid")
-    .notNull()
-    .references(() => collector.uid, { onDelete: "cascade" }),
-});
+export const wii = pgTable(
+  "wii",
+  {
+    macAddress: text("mac_address").primaryKey(),
+    nickname: text("nickname").notNull(),
+    ipAddress: text("ip_address").notNull(),
+    collectorUID: text("collector_uid")
+      .notNull()
+      .references(() => collector.uid, { onDelete: "cascade" }),
+  },
+  (table) => [
+    check(
+      "ip_address_is_valid",
+      sql`${table.ipAddress} ~* '^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$'`,
+    ),
+    check(
+      "mac_address_is_valid",
+      sql`${table.macAddress} ~* '^([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2})$'`,
+    ),
+    index("collector_uid_idx").on(table.collectorUID),
+  ],
+);
