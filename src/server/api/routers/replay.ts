78df998380c36a4e44cce42ded062835896c9f2f
Andreas Rossa <andreas@rossamail.de>
2025-04-06 00:32:29 +0200

    Add more replay routes

--- src/server/api/routers/replay.ts
+++ src/server/api/routers/replay.ts
@@ -1,11 +1,14 @@
 import {
   createTRPCRouter,
   protectedCollectorProcedure,
+  protectedProcedure,
 } from "@/server/api/trpc";
-import { db } from "@/server/db";
 import { replay } from "@/server/db/schema";
+import { minioClient } from "@/server/utils/minio";
 import { TRPCError } from "@trpc/server";
+import { and, arrayContains, eq, gte, lte } from "drizzle-orm";
 import { z } from "zod";
+import { zfd } from "zod-form-data";
 
 const replayBucket = "replays";
 const replayFileSizeLimit = 50 * 1024 * 1024; // 50MB
@@ -23,7 +26,7 @@ export const replayRouter = createTRPCRouter({
       }),
     )
     .mutation(async ({ ctx, input }) => {
-      const [replayEntry] = await db
+      const [replayEntry] = await ctx.db
         .insert(replay)
         .values({
           startedAt: input.startedAt,
@@ -44,24 +47,114 @@ export const replayRouter = createTRPCRouter({
     }),
   end: protectedCollectorProcedure
     .input(
-      z.object({
-        replayId: z.number(),
+      zfd.formData({
+        replayId: zfd.numeric(),
+        replayFile: zfd.file(),
       }),
     )
     .mutation(async ({ ctx, input }) => {
-      // const bucketExists = await minioClient.bucketExists(replayBucket);
-      // if (!bucketExists) {
-      //   await minioClient.makeBucket(replayBucket);
-      // }
-      // const fileMetadata = {
-      //   "Content-Type": "binary/octet-stream",
-      // };
-      // const uploadedFile = await minioClient.putObject(
-      //   replayBucket,
-      //   file.name,
-      //   Buffer.from(await file.arrayBuffer()),
-      //   file.size,
-      //   fileMetadata,
-      // );
+      const [replayEntry] = await ctx.db
+        .select()
+        .from(replay)
+        .where(eq(replay.id, input.replayId));
+
+      if (!replayEntry) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Replay not found",
+        });
+      }
+
+      const bucketExists = await minioClient.bucketExists(replayBucket);
+
+      if (!bucketExists) {
+        await minioClient.makeBucket(replayBucket);
+      }
+
+      const fileMetadata = {
+        "Content-Type": "binary/octet-stream",
+      };
+
+      const uploadedFile = await minioClient.putObject(
+        replayBucket,
+        input.replayFile.name,
+        Buffer.from(await input.replayFile.arrayBuffer()),
+        input.replayFile.size,
+        fileMetadata,
+      );
+
+      await ctx.db
+        .update(replay)
+        .set({
+          fileId: uploadedFile.etag,
+          isFinished: true,
+        })
+        .where(eq(replay.id, input.replayId));
+    }),
+  get: protectedProcedure
+    .input(z.object({ replayId: z.number() }))
+    .query(async ({ input, ctx }) => {
+      const [replayEntry] = await ctx.db
+        .select()
+        .from(replay)
+        .where(eq(replay.id, input.replayId));
+
+      if (!replayEntry) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Replay not found",
+        });
+      }
+
+      return replayEntry;
+    }),
+  list: protectedProcedure
+    .input(
+      z.object({
+        collectorUID: z.string().optional(),
+        stageId: z.number().optional(),
+        characterIds: z.array(z.number()).optional(),
+        isFinished: z.boolean().optional(),
+        startedAfter: z.coerce.date().optional(),
+        startedBefore: z.coerce.date().optional(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const filterConditions = [];
+
+      if (input.collectorUID) {
+        filterConditions.push(eq(replay.collectorUID, input.collectorUID));
+      }
+
+      if (input.stageId) {
+        filterConditions.push(eq(replay.stageId, input.stageId));
+      }
+
+      if (input.characterIds) {
+        filterConditions.push(
+          arrayContains(replay.characterIds, input.characterIds),
+        );
+      }
+
+      if (input.isFinished) {
+        filterConditions.push(eq(replay.isFinished, input.isFinished));
+      }
+
+      if (input.startedAfter) {
+        filterConditions.push(gte(replay.startedAt, input.startedAfter));
+      }
+
+      if (input.startedBefore) {
+        filterConditions.push(lte(replay.startedAt, input.startedBefore));
+      }
+
+      const replayEntries = await ctx.db
+        .select()
+        .from(replay)
+        .where(
+          filterConditions.length > 0 ? and(...filterConditions) : undefined,
+        );
+
+      return replayEntries;
     }),
 });
