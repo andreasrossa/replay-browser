1d8a9ef7e82ae182bb55214d42c05c75d8ee86e5
Andreas Rossa <andreas@rossamail.de>
2025-05-18 22:23:13 +0200

    Add vercel speed insights to authenticated layout

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -11,6 +11,7 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function ReplaysLayout({
   children,
@@ -18,22 +19,25 @@ export default function ReplaysLayout({
   children: React.ReactNode;
 }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">Replays</BreadcrumbLink>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </header>
-        <main className="p-8">{children}</main>
-      </SidebarInset>
-    </SidebarProvider>
+    <>
+      <SpeedInsights />
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">Replays</BreadcrumbLink>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </header>
+          <main className="p-8">{children}</main>
+        </SidebarInset>
+      </SidebarProvider>
+    </>
   );
 }
