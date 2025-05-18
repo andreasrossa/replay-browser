19529eb55cfb5a9ada8683ff2eb29310fe895196
Andreas Rossa <andreas@rossamail.de>
2025-05-18 23:53:21 +0200

    fix dumb error

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,12 +1,7 @@
-import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 import ErrorPageContent from "./_components/error-content";
 
 export default function ErrorPage() {
-  const params = useSearchParams();
-
-  console.log(params);
-
   return (
     <div className="flex h-full w-full items-center justify-center">
       <Suspense fallback={<div>Loading...</div>}>
