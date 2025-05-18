2691b223dc9d842e4c08993c00725123260f680e
Andreas Rossa <andreas@rossamail.de>
2025-05-18 21:13:48 +0200

    redirect to login page from start page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex h-screen w-full items-center justify-center">
-      <div className="flex flex-col items-center gap-4">
-        <h1 className="inline-block bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 bg-clip-text text-6xl font-black text-transparent">
-          I got that sh*t recorded
-        </h1>
-        <h2 className="text-2xl">Never loose another cool combo again.</h2>
-        <Button asChild>
-          <Link href="/login">Get Started</Link>
-        </Button>
-      </div>
-    </main>
-  );
+  redirect("/login");
 }
